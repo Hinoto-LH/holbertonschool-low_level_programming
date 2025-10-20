e1aada7126d1f5be48f88d55414f5f5df5394fb6
« <>
2025-10-20 16:28:55 +0000

    size of

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,19 +7,16 @@
 */
 int main(void)
 {
-	printf("Size of a char: 1  byte\n");
-	printf("Size of an int: 4  byte(s)\n");
-	printf("Size of a long int: 4 byte(s)\n");
-	printf("Size of a long long int: 8 byte(s)\n");
-	printf("Size of a float: 4 byte(s)\n");
+	printf("Size32 of a char: 1  byte\n");
+	printf("Size32 of an int: 4  byte(s)\n");
+	printf("Size32 of a long int: 4 byte(s)\n");
+	printf("Size32 of a long long int: 8 byte(s)\n");
+	printf("Size32 of a float: 4 byte(s)\n");
+
+	printf("Size64 of a char: 1  byte\n");
+        printf("Size64 of an int: 4  byte(s)\n");
+        printf("Size64 of a long int: 8  byte(s)\n");
+        printf("Size64 of a long long int: 8 byte(s)\n");
+        printf("Size64 of a float: 4 byte(s)\n");
 	return (0);
-}
-int main(void)
-{
- printf("Size of a char: 1  byte\n");
-        printf("Size of an int: 4  byte(s)\n");
-        printf("Size of a long int: 8  byte(s)\n");
-        printf("Size of a long long int: 8 byte(s)\n");
-        printf("Size of a float: 4 byte(s)\n");
-        return (0);
 }
