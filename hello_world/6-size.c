deaede797a75928889a595a638adcf2fa7093515
« <>
2025-10-20 16:30:23 +0000

    size of

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,13 +7,13 @@
 */
 int main(void)
 {
-	printf("Size32 of a char: 1  byte\n");
+	printf("Size32 of a char: 1  byte(s)\n");
 	printf("Size32 of an int: 4  byte(s)\n");
 	printf("Size32 of a long int: 4 byte(s)\n");
 	printf("Size32 of a long long int: 8 byte(s)\n");
 	printf("Size32 of a float: 4 byte(s)\n");
 
-	printf("Size64 of a char: 1  byte\n");
+	printf("Size64 of a char: 1  byte(s)\n");
         printf("Size64 of an int: 4  byte(s)\n");
         printf("Size64 of a long int: 8  byte(s)\n");
         printf("Size64 of a long long int: 8 byte(s)\n");
