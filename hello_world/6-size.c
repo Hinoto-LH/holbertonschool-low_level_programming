67329675d628369da22bf3e71273877c03e2361c
« <>
2025-10-20 16:01:36 +0000

    size of

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Size of a char %lu byte(s)\n");
+	printf("Size of an int: %lu byte(s)\n");
+	printf("Size of a long int: %lu byte(s)\n");
+	printf("Size of a long longint: %lu byte(s)\n");
+	printf("Size of a float: %lu byte(s)\n");
+	return (0);
+}
