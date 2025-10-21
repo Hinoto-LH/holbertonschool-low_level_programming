052dc131630bccb88d6d9176e99d288b48693884
« <>
2025-10-21 14:12:03 +0000

    if, if else, else

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main - n is positive, negative or zero
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
