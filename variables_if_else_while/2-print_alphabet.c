c45ff014873a78ddb89fea277e9cf44d39f185a8
« <>
2025-10-21 15:53:24 +0000

    alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,11 +2,12 @@
 
 int main(void)
 {
-	char alphabet = 'a'
-		while (alphabet <= 'z')
+	char alphabet = 'a';
+		while(alphabet <= 'z')
 		{
 		putchar(alphabet);
 		alphabet++;
 		}
+		putchar('\n');
 	return (0);
 		}
