536d58dfa9c20794b110e77bdf6502de65240972
« <>
2025-10-21 08:34:29 +0000

    %d decimal

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,10 @@
 */
 int main(void)
 {
-	printf("Size of a char: %d  byte(s), sizeof (char)\n");
-	printf("Size32 of an int: %d byte(s), sizeof (int)\n");
-	printf("Size32 of a long int: %d byte(s), sizeof (long int)\n");
-	printf("Size32 of a long long int: %d byte(s), sizeof (long long int)\n");
-	printf("Size32 of a float: %d byte(s), sizeof (float)\n");
+	printf("Size of a char = %d  byte(s), sizeof (char)\n");
+	printf("Size32 of an int = %d byte(s), sizeof (int)\n");
+	printf("Size32 of a long int = %d byte(s), sizeof (long int)\n");
+	printf("Size32 of a long long int = %d byte(s), sizeof (long long int)\n");
+	printf("Size32 of a float = %d byte(s), sizeof (float)\n");
 	return (0);
 }
