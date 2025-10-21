e2a78d6d7656fa3c38f9b97b882224621659a321
« <>
2025-10-21 15:01:49 +0000

    % 10 ajoute un remainder

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,11 +6,10 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+	int n, x;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	x = n % 10;
-	int x;
 	if (n > 5)
 	{
 		printf("the last digit of %d and %d is greater than 5\n", n, x);
