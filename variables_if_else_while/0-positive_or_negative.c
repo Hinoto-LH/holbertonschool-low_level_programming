6524511d1fb7b2bb41b02ceac6517bd87ba3c4a1
« <>
2025-10-21 13:43:16 +0000

    if, if else, else

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,17 +12,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	
        	printf(" %d \n", n);
-	if (n > 0)
+	if(n > 0)
 	{
 		printf("the number is positif\n");
 	}
-	else if (n > 0)
+	else if(n > 0)
 		{
 			printf("the number is negative\n");
 		}
 	else
 	{
-		printf("the number is zero\n");
+		printf("the number is 0\n");
 	}
 	return (0);
 }
