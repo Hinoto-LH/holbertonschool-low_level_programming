ebb3c41bfaa500d60eaa813778bd73a86c6bc00a
« <>
2025-10-24 08:57:11 +0000

    nested loop

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,14 @@
 #include"main.h"
+
+void print_alphabet_x10(void)
 {
     char print_alphabet_x10;
     char print;
 		for(print_alphabet_x10='0'; print_alphabet_x10 <= '9'; print_alphabet_x10++) {
-		    putchar(print_alphabet_x10);
             for(print = 'a'; print <= 'z'; print++) {
-                putchar(print);
+                _putchar(print);
             }
             
-         putchar('\n');   	
+         _putchar('\n');   	
 		}
 }
