18da1817f949b699d1c613664eb7cab17e87c948
« <>
2025-11-02 10:59:08 +0000

    lengt

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -5,13 +5,13 @@ void puts_half(char *str)
 	int length = 0;
 	int n;
 
-	while(str[length] != '\0')
+	while (str[length] != '\0')
 	{
 		length++;
 	}
 	n = (length + 1) / 2;
-	
-	while(n < length)
+
+	while (n < length)
 	{
 		_putchar(str[n]);
 		n++;
