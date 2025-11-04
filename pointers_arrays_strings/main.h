e19ac34998281c98a38fc70bfaf72721cb9c22f5
Hinoto <hinoto@MacBook-Air-de-Hinoto.local>
2025-11-04 14:21:35 +0100

    3 task

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,10 +1,8 @@
 #ifndef main_h
 #define main_h
 
-
-
-
-
+void print_chessboard(char (*a)[8]);
+char *_strpbrk(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
