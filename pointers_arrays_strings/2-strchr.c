9706c21bf281acd589b40a98a99745250aa7570d
« <>
2025-11-07 14:14:44 +0000

    Betty

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,13 @@
 char *_strchr(char *s, char c)
 {
 
-    while(*s)
-    {
-    if (*s == c)
-       return (s);
-     s++;
-    }
-    if (*s == '\0')
-       return(s);
-
-    return(NULL);
+	while (*s)
+	{
+		if (*s == c)
+		return (s);
+	s++;
+	}
+	if (*s == '\0')
+	return (s);
+	return (NULL);
 }
