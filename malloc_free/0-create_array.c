8e9ae423653727b731866b39b77ea64b520da32e
Hinoto <hinoto@MacBook-Air-de-Hinoto.local>
2025-11-11 17:04:42 +0100

    fix malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -19,16 +19,19 @@ char *create_array(unsigned int size, char c)
     {
         return NULL;
     }
-     tab = malloc(sizeof(size));
-     free(tab);
+
+    tab = malloc(sizeof(char) * size);
+
     if (tab == NULL)
     {
+        exit(1);
         return(0);
     }
-    
+
     for (i = 0; i < size; i++)
     {
-        tab [i] = c;
+        tab[i] = c;
     }
+
     return tab;
 }
