f96924629f057e8d9b56b6312900f7e3f46e51ad
Hinoto-LH <masseron.a.pro@gmail.com>
2025-11-11 19:49:24 +0100

    Concat 3

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -4,10 +4,11 @@
 #include <string.h>
 
 /**
- * *_strdup - copies the string given as parameter
- * @str: string to duplicate
+ * *str_concat - concatenates two strings
+ * @s1: string to concatenate
+ * @s2: other string to concatenate
  *
- * Return: pointer to the copied string (Success), NULL (Error)
+ * Return: pointer to the new string created (Success), or NULL (Error)
  */
 char *str_concat(char *s1, char *s2)
 {
