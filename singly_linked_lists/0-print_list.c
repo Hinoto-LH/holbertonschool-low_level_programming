fab2e6cf4dba9e4aab17a7afb4fd2feccf602cab
Hinoto-LH <masseron.a.pro@gmail.com>
2025-12-02 13:14:35 +0100

    Added 1st task

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,14 +3,18 @@
 
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	size_t elements = 0;
 
-	if (*str == NULL)
+	while (h != NULL)
 	{
-		printf ("[0] (nil)\n", str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		elements++;
+		h = h->next;
 	}
-	else
-	{
-		count++;
-	}
+
+	return (elements);
 }
