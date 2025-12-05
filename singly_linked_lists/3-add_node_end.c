0656eb9fc281f0a6e7589b8429737a8affe0a286
Hinoto-LH <masseron.a.pro@gmail.com>
2025-12-05 14:24:39 +0100

    Added task 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,29 +1,53 @@
 #include "lists.h"
 
+/**
+* add_node_end - adds a new node at the ending of a linked list
+* @head: adds a new node at the beginning of a linked list
+* @str: string
+*
+* return: the address of the new element, or NULL if it fails
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *element;
+	list_t *newNode;
+	list_t *temp = *head;
+	unsigned int len = 0;
 
-	if (element == NULL)
+	 /* calculer la longueur sans strlen */
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	/* Allocation de mémoire de la nouvelle node */
+	newNode = malloc(sizeof(list_t));
+
+	if (newNode == NULL)
 	{
 		return (NULL);
 	}
+	/* dupliquer la chaîne demandée */
+	newNode->str = strdup(str);
 
-	element->value = str;
-	element->next = NULL;
+	/* longueur calculer manuellement */
+	newNode->len = len;
 
-	if (head)
+	/* le nouveau node sera le dernier */
+	newNode->next = NULL;
+
+	/* Si la liste chaînée est vide, définissez le nouveau nœud comme tête */
+	if (*head == NULL)
 	{
-	return (element);
+		*head = newNode;
+		return (newNode);
 	}
-
-	list_t *temp;
-	temp = str;
 
 	while (temp->next != NULL)
 	{
 		temp = temp->next;
 	}
-	temp->next = element;
-	return (head);
+	/* la tête pointe maintenant sur le nouveau */
+	temp->next = newNode;
+
+	return (newNode);
 }
