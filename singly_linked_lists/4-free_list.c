f5acb78d2f6e8fcadc4041555fc70e7d6a326f66
Hinoto-LH <masseron.a.pro@gmail.com>
2025-12-05 14:43:39 +0100

    Added task 4

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
 void free_list(list_t *head)
 {
-    
+	list_t *temp;
+
+    /* parcourt la liste */
+	while (head)
+	{
+/* sauvegarde du node suivant dans temp */
+		temp = head->next;
+    /* libère la node copié */
+		free(head->str);
+    /* puis libère le noeud lui-même */
+		free(head);
+    /* et on avance */
+		head = temp;
+	}
 }
