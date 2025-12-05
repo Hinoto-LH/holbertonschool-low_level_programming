cdf18126af6436f29b039147138cf030f1d1b695
Hinoto-LH <masseron.a.pro@gmail.com>
2025-12-05 11:34:27 +0100

    Fixed 2nd task

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,15 +1,22 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: adds a new node at the beginning of a linked list
- *
- * return: the address of the new element, or NULL if it fails
- */
+* add_node - adds a new node at the beginning of a linked list
+* @head: adds a new node at the beginning of a linked list
+*
+* return: the address of the new element, or NULL if it fails
+*/
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode;
+	unsigned int len = 0;
 
+	 /* calculer la longueur sans strlen */
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	
 	/* Allocation de mémoire de la nouvelle node */
 	newNode = malloc(sizeof(list_t));
 
